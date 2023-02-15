de7e96e0ede84b4afea83e2fb855c6e34173591c
Michael Gastello <michaelgastello@aol.com>
2023-02-15 17:29:32 -0500

    finished create method

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,13 +1,27 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
+    @users = User.all
+    render json: @users
+    # render plain: "I'm in the index action!"
   end
 
   def create
-    render json: params
+    user = User.new(params.require(:user).permit(:name, :email))
+    # User.new(params[:user])
+
+    # if @user.save
+    #     redirect_to user_url(@user)
+    # else
+    #     render json: @user.errors.full_messages, status: 422
+    # end
+    user.save!
+    render json: user
   end
 
   def show
-    render json: params
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
   end
 end
