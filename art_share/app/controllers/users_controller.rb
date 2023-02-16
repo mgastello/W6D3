6762d18512c143c8bd24eb144434ba3fb0f71a9c
Olga <olga.sleepless@gmail.com>
2023-02-16 17:31:16 -0500

    Finish phase 1 and 2

--- art_share/app/controllers/users_controller.rb
+++ art_share/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token
+    def index
+        @users = User.all
+        render json: @users
+    end
 
     def create
         user = User.new(user_params)
@@ -11,6 +15,33 @@ class UsersController < ApplicationController
             render json: user.errors.full_messages, status: 422
         end
     end
+
+    def destroy
+        @user = User.find(params[:id])
+
+        if @user.destroy
+            render json: 'user DESTROYED!'
+        else
+            render json: @user.errors.full_messages, status: 422 
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+        
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to user_url(@user.id)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end 
+    end
+
 
     private
 
--- art_share/app/models/artwork.rb
+++ art_share/app/models/artwork.rb
@@ -27,7 +27,8 @@ class Artwork < ApplicationRecord
   has_many :artwork_shares,
     primary_key: :id,
     foreign_key: :artwork_id,
-    class_name: :ArtworkShare
+    class_name: :ArtworkShare,
+    dependent: :destroy
 
   has_many :shared_viewers,
     through: :artwork_shares,
